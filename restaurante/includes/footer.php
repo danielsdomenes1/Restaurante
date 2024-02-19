fd52e73fbdbe7219f0169e0caa157be58b0e547f
DANIEL SANCHES DOMENES <daniel.sdomenes1@senacsp.edu.br>
2024-02-19 17:21:54 -0300

    mudando estrutura do html , css colocando códigos pra ficar responsivo

---
+++ restaurante/includes/footer.php
@@ -0,0 +1,48 @@
+<footer id="footer" class="small-12 columns no-padding">
+
+<div class="global-page-container">
+
+    <div class="small-11 small-centered large-12 columns footer-section">
+
+        <div class="follow-us small-5 medium-3 small-offset-1 medium-offset-0 columns">
+            <h4 class="footer-section-title">Siga-nos</h4>
+            <a href="http://www.facebook.com"><img src="img/social-icons/facebook.svg" alt="facebook-icon"></a>
+            <a href="http://www.twitter.com"><img src="img/social-icons/twitter.svg" alt="facebook-icon"></a>
+            <a href="http://www.instagram.com"><img src="img/social-icons/instagram.svg" alt="facebook-icon"></a>
+        </div>
+        
+        <div class="contato small-5 medium-3 small-offset-1 medium-offset-0 columns">
+            <h4 class="footer-section-title">Contato</h4>
+            <p>
+                Avenida Conselheiro Nébias, 308<br>
+                Santos/SP<br>
+                T. 13-3633-3333<br>
+                contato@restobar.com.br
+            </p>
+        </div>
+        
+        <div class="horario small-5 medium-3 small-offset-1 medium-offset-0 columns">
+            <h4 class="footer-section-title">Horários</h4>
+            <p><span class="horario-fechado'">Fechado Agora</span><br>
+            Seg-Sex: 11h30 - 24h00<br>
+            Sábado 11h30 - 02h00<br>
+            Domingo 11h30 - 18h</p>
+        </div>
+        
+        <div class="como-chegar small-5 medium-3 small-offset-1 medium-offset-0 columns">
+            <h4 class="footer-section-title">Como chegar</h4>
+            <div id="map"></div>
+        </div>
+        
+        <hr></hr>
+        
+        <div class="copyright small-12 columns">
+        
+            2022 &copy; Todos os direitos reservados
+        
+        </div>
+    </div>
+
+</div>
+
+</footer>
